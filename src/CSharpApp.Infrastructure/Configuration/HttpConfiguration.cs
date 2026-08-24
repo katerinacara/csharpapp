ee137cf3ad926ac7de091ae00358ce987aa76f4e
katerinacara <katerinaa014@gmail.com>
2026-08-25 00:30:24 +0300

    refactor(http): configure RestApi client base address

--- src/CSharpApp.Infrastructure/Configuration/HttpConfiguration.cs
+++ src/CSharpApp.Infrastructure/Configuration/HttpConfiguration.cs
@@ -16,20 +16,25 @@ public static class HttpConfiguration
         var httpSettings = configuration
            .GetSection(nameof(HttpClientSettings))
            .Get<HttpClientSettings>()!;
+       
+        var restApiSettings = configuration
+            .GetSection(nameof(RestApiSettings))
+            .Get<RestApiSettings>()!;
 
         services.AddSingleton<ITokenProvider, TokenProvider>();
-
         services.AddTransient<JwtAuthenticationHandler>();
 
-        services.AddHttpClient("RestApi")
-            .AddTransientHttpErrorPolicy(policy =>
-                policy.WaitAndRetryAsync(
-                    httpSettings.RetryCount,
-                    retryAttempt => TimeSpan.FromMilliseconds(
-                        httpSettings.SleepDuration)))
+        services.AddHttpClient("RestApi", client =>
+        {
+            client.BaseAddress = new Uri(restApiSettings.BaseUrl!);
+        })
+        .AddTransientHttpErrorPolicy(policy =>
+            policy.WaitAndRetryAsync(
+                httpSettings.RetryCount,
+                retryAttempt => TimeSpan.FromMilliseconds(
+                    httpSettings.SleepDuration)))
         .AddHttpMessageHandler<JwtAuthenticationHandler>();
 
-        
 
         return services;
     }
