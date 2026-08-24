64a221eae5f22f25fd236c67bc391c693d483567
katerinacara <katerinaa014@gmail.com>
2026-08-24 21:22:36 +0300

    refactor: configure HTTP client factory with retry policy

--- src/CSharpApp.Infrastructure/Configuration/DefaultConfiguration.cs
+++ src/CSharpApp.Infrastructure/Configuration/DefaultConfiguration.cs
@@ -5,14 +5,13 @@ public static class DefaultConfiguration
     public static IServiceCollection AddDefaultConfiguration(this IServiceCollection services)
     {
         var serviceProvider = services.BuildServiceProvider();
-        var configuration = serviceProvider.GetService<IConfiguration>();
+        var configuration = serviceProvider.GetRequiredService<IConfiguration>();
 
-        services.Configure<RestApiSettings>(configuration!.GetSection(nameof(RestApiSettings)));
+        services.Configure<RestApiSettings>(configuration.GetSection(nameof(RestApiSettings)));
         services.Configure<HttpClientSettings>(configuration.GetSection(nameof(HttpClientSettings)));
 
-        //services.AddSingleton<IProductsService, ProductsService>();
-        services.AddHttpClient<IProductsService, ProductsService>();
-        
+        services.AddScoped<IProductsService, ProductsService>();
+
         return services;
     }
 }
